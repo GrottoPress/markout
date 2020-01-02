695c15bae5e8a08a601d9c893b4a3b9ad9ae79f2
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-01-02 22:12:24 +0000

    Eliminate global variable

--- README.md
+++ README.md
@@ -233,15 +233,15 @@ A component may accept a block, or named arguments, or both:
 struct MyLinkComponent < BaseComponent
   @r : String
 
-  def initialize(@url : String, **opts, &b : Proc(Markout::HTML, Nil))
-    @r = render(**opts, &b)
+  def initialize(url : String, **opts, &b : Proc(Markout::HTML, Nil))
+    @r = render(url, **opts, &b)
   end
 
   private def render(m : Markout::HTML) : Nil
     m.raw @r
   end
 
-  private def render(**opts, & : Proc(Markout::HTML, Nil))
+  private def render(url : String, **opts, & : Proc(Markout::HTML, Nil)) : String
     yield (a = Markout.html html_version)
     args = opts.merge({href: @url})
     args = {class: "link"}.merge args
--- spec/_support/my_link_component.cr
+++ spec/_support/my_link_component.cr
@@ -1,22 +1,29 @@
 struct MyLinkComponent < BaseComponent
   @r : String
 
-  def initialize(@url : String, **opts, &b : Proc(Markout::HTML, Nil))
-    @r = render(**opts, &b)
+  def initialize(url : String, **opts, &b : Proc(Markout::HTML, Nil))
+    @r = render(url, **opts, &b)
   end
 
   private def render(m : Markout::HTML) : Nil
     m.raw @r
   end
 
-  private def render(**opts, & : Proc(Markout::HTML, Nil)) : String
+  private def render(
+    url : String,
+    **opts,
+    & : Proc(Markout::HTML, Nil)
+  ) : String
     yield (a = Markout.html html_version)
-    args = opts.merge({href: @url})
+
+    args = opts.merge({href: url})
     args = {class: "link"}.merge args
+
     m = Markout.html html_version
     m.a **args do |m|
       m.raw(a)
     end
+
     m.to_s
   end
 end
